361814b4005acaa8b2b9d31ba76223aacf6b1e60
laonayt <laonayt@163.com>
2020-03-11 15:31:56 +0800

    init

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/we/Flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/we/Downloads/flutter_easyrefresh-2/example"
+export "FLUTTER_TARGET=/Users/we/Downloads/flutter_easyrefresh-2/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/we/Flutter/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=2.0.9"
+export "FLUTTER_BUILD_NUMBER=36"
+export "TRACK_WIDGET_CREATION=true"
